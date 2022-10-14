d87ee4b55a235f9726fd3a64b1fa358af37ef38c
Thad <thad888@hotmail.com>
2022-10-14 02:05:24 -0700

    Add button effects and red/black colour layout

--- script.js
+++ script.js
@@ -1,12 +1,89 @@
 const gridArea = document.getElementById("gridArea");
+const sizeButton = document.getElementById("size");
+const resetButton = document.getElementById("reset");
+const rainbowButton = document.getElementById("rainbow");
 
-numberOfBoxes = 16*16;
+sizeButton.onclick = newGridSize;
+resetButton.onclick = resetGrid;
+rainbowButton.onclick = rainbowGrid;
+
+let row = 16;
+let gridSize = row*row;
+let reset = false;
+let rainbow = false;
+
+makeGrid();
+
 function makeGrid(){
-    /*console.log("makeGrid");*/
-    for (i = 0; i<numberOfBoxes; i++){
+    removeCurrentGrid();
+    //In the css file, the variable '--grid-size' is used to make the correct number of rows/columns
+    gridArea.style.setProperty('--grid-size',row);
+
+    for (i = 0; i<gridSize; i++){
         let box = document.createElement("div");
-        gridArea.appendChild(box).className = "box"
+        //The default box colour is light grey
+        if(reset) box.style.backgroundColor = "lightgrey";
+
+        //In rainbow mode, the boxes will change to a random colour 
+        if(rainbow){
+            box.addEventListener('mouseenter', () => box.style.backgroundColor = getRandomColour());
+            gridArea.appendChild(box).className = "box";
+        }
+        //In normal mode, the boxes will change to dark grey
+        else{
+            box.addEventListener('mouseenter', () => box.style.backgroundColor = "#3f3e3e");
+            gridArea.appendChild(box).className = "box";
+        }
     }
 }
 
-makeGrid();
+//Input must be a number from 1 to 100
+function getUserInput(){
+    let row = prompt("Enter a number for the row/column size")
+    if(isNaN(row)){
+        alert("Please enter a valid number")
+        row = getUserInput();
+    }  
+    if (!(Number(row)<=100) || !(Number(row)>0)){
+        alert("Please enter a number from 1 to 100")
+        row = getUserInput();
+    }
+    return (Number(row));
+}
+
+function getRandomColour(){
+    let values = '0123456789ABCDEF';
+    let colour = '#';
+
+    for(let i=0; i<6; i++){
+        colour += values[Math.floor(Math.random()*16)];
+    }
+    return colour;
+}
+
+function removeCurrentGrid(){
+    while (gridArea.firstChild) {
+        gridArea.removeChild(gridArea.lastChild);
+    }
+}
+
+function newGridSize(){
+    row = getUserInput();
+    gridSize = row*row;
+    makeGrid();
+}
+
+function resetGrid(){
+    reset = true;
+    removeCurrentGrid();
+    makeGrid();
+    reset = false;
+}
+
+function rainbowGrid(){
+    rainbow = true;
+    resetGrid();
+    rainbow = false;
+}
+
+
